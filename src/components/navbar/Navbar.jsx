88c08526505242db5a3952286ca6abe20d16f126
chrisclarkedev <cclarke8904@gmail.com>
2022-11-22 18:30:10 -0500

    asset images

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
 import './navbar.css';
 
+// Usage of BEM "Block Element Modifier"
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="rwrks__navbar">
+      <div className="rwrks__navbar-links">
+        <div className="rwrks__navbar-links_logo">
+          <img src={logo} alt="logo" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
