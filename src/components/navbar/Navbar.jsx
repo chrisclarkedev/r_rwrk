3082f09c5d5b64cea7b2381ca3356c92d3a4b516
chrisclarkedev <cclarke8904@gmail.com>
2022-11-23 15:10:38 -0500

    navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -11,6 +11,15 @@ const Navbar = () => {
         <div className="rwrks__navbar-links_logo">
           <img src={logo} alt="logo" />
         </div>
+        <div className="rwrks__navbar-links_container">
+          <p>
+            <a href="#home">Home</a>
+            <a href="#rwkrs">What is RWRKS?</a>
+            <a href="#possible">Open AI</a>
+            <a href="#features">Case Study</a>
+            <a href="#blog">Library</a>
+          </p>
+        </div>
       </div>
     </div>
   );
