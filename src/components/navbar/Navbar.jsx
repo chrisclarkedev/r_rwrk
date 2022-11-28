8bd35d83dc91ef0c98b2a37ba2ca543b442b19fa
chrisclarkedev <cclarke8904@gmail.com>
2022-11-28 15:36:12 -0500

    menu component, navbar styling changes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
-import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
+import React, { useState } from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import logo from '../../assets/logo.png';
 import './navbar.css';
 
+// Creation of Menu component to be used in both desktop and mobile viewing
+const Menu = () => (
+  <>
+    <p>
+      <a href="#home">Home</a>
+    </p>
+    <p>
+      <a href="#rwkrs">What is RWRKS?</a>
+    </p>
+    <p>
+      <a href="#possible">Open AI</a>
+    </p>
+    <p>
+      <a href="#features">Case Study</a>
+    </p>
+    <p>
+      <a href="#blog">Library</a>
+    </p>
+  </>
+);
+
 // Usage of BEM "Block Element Modifier"
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
     <div className="rwrks__navbar">
       <div className="rwrks__navbar-links">
@@ -12,22 +35,34 @@ const Navbar = () => {
           <img src={logo} alt="logo" />
         </div>
         <div className="rwrks__navbar-links_container">
-          <p>
-            <a href="#home">Home</a>
-          </p>
-          <p>
-            <a href="#rwkrs">What is RWRKS?</a>
-          </p>
-          <p>
-            <a href="#possible">Open AI</a>
-          </p>
-          <p>
-            <a href="#features">Case Study</a>
-          </p>
-          <p>
-            <a href="#blog">Library</a>
-          </p>
+          <Menu />
         </div>
+      </div>
+      <div className="rwrks__navbar-sign">
+        <p>Sign in</p>
+        <button type="button">Sign up</button>
+      </div>
+      <div className="rwrks__navbar-menu">
+        {toggleMenu ? (
+          <RiCloseLine
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(false)}
+          />
+        ) : (
+          <RiMenu3Line
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(true)}
+          />
+        )}
+        {toggleMenu && (
+          <div className="rwrks__navbar-menu_container scale-up-center">
+            <div className="rwrks__navbar-menu_container-links">
+              <Menu />
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
