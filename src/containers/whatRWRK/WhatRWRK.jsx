1d7a212efb46b5e1235e8cd29afeabd181cb9f85
chrisclarkedev <cclarke8904@gmail.com>
2022-12-01 19:55:29 -0500

    feature component started

--- src/containers/whatRWRK/WhatRWRK.jsx
+++ src/containers/whatRWRK/WhatRWRK.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import { Feature } from '../../components';
 import './whatRWRK.css';
 
 const WhatRWRK = () => {
-  return <div>WhatRWRK</div>;
+  return (
+    <div className="rwrks__whatrwrks section__margin" id="rwrks">
+      <div className="rwrks__whatrwrks-feature">
+        <Feature />
+      </div>
+      <div className="rwrks__whatrwkrs-heading">
+        <h1 className="gradient-text">
+          Everything you have imagined is now possible
+        </h1>
+        <p>Explore the library</p>
+      </div>
+      <div className="rwrks__whatrwkrs-container">
+        <Feature />
+        <Feature />
+        <Feature />
+      </div>
+    </div>
+  );
 };
 
 export default WhatRWRK;
