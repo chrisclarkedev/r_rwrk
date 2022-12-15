8b3400e623f97107b34a6cebbe3e1eccafb577cd
chrisclarkedev <cclarke8904@gmail.com>
2022-12-15 12:00:53 -0500

    blog styling and media queries

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import './article.css';
 
-const Article = ({ imgUrl }) => {
+const Article = ({ imgUrl, date, title }) => {
   return (
     <div className="rwrks__blog-container_article">
       <div className="rwrks__blog-container_article-image">
-        <img src={imgUrl} alt="blog image" />
+        <img src={imgUrl} alt="blog" />
+      </div>
+      <div className="rwrks__blog-container_article-content">
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        <p>READ FULL ARTICLE</p>
       </div>
     </div>
   );
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -7,19 +7,37 @@ const Blog = () => {
   return (
     <div className="rwrks__blog section__padding" id="blog">
       <div className="rwrks__blog-heading">
-        <h1 className="gradient__text">
-          Whatever is happening, We blog about it.
-        </h1>
+        <h1 className="gradient__text">Whatever happens, We blog about it.</h1>
       </div>
-      <div className="rwrks__blog-cntainer">
+      <div className="rwrks__blog-container">
         <div className="rwrks__blog-container_groupA">
-          <Article imgUrl={blog01} />
+          <Article
+            imgUrl={blog05}
+            date="Dec 13, 2022"
+            title="RWRKS and Open AI is the future. Let's explore how it works."
+          />
         </div>
         <div className="rwrks__blog-container_groupB">
-          <Article imgUrl={blog02} />
-          <Article imgUrl={blog03} />
-          <Article imgUrl={blog04} />
-          <Article imgUrl={blog05} />
+          <Article
+            imgUrl={blog02}
+            date="Dec 13, 2022"
+            title="RWRKS and Open AI is the future. Let's explore how it works."
+          />
+          <Article
+            imgUrl={blog01}
+            date="Dec 13, 2022"
+            title="RWRKS and Open AI is the future. Let's explore how it works."
+          />
+          <Article
+            imgUrl={blog04}
+            date="Dec 13, 2022"
+            title="RWRKS and Open AI is the future. Let's explore how it works."
+          />
+          <Article
+            imgUrl={blog03}
+            date="Dec 13, 2022"
+            title="RWRKS and Open AI is the future. Let's explore how it works."
+          />
         </div>
       </div>
     </div>
